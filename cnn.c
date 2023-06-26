9bcf40dabcbd9782a2464127e374ec35720c35ee
Khawaja Shaheryar <58368527+RNGesus-exe@users.noreply.github.com>
2023-06-26 08:31:16 +0000

    Completed Lenet-5 Architecture

--- cnn.c
+++ cnn.c
@@ -1,45 +1,100 @@
 #include "cnn.h"
 
+void log_softmax(float input[NUM_FILTERS_7], int input_size, float output[NUM_FILTERS_7])
+{
+    // Find the maximum value in the input array
+    float max_value = input[0];
+    for (int i = 1; i < input_size; i++)
+    {
+        if (input[i] > max_value)
+        {
+            max_value = input[i];
+        }
+    }
+
+    // Compute the softmax values
+    float sum_exp = 0.0;
+    for (int i = 0; i < input_size; i++)
+    {
+        output[i] = exp(input[i] - max_value);
+        sum_exp += output[i];
+    }
+
+    // Compute the log softmax values
+    for (int i = 0; i < input_size; i++)
+    {
+        output[i] = log(output[i] / sum_exp);
+    }
+}
+
 // Forward pass through the LeNet-5 model
 void forwardPass(LeNet5 *model, float input[INPUT_ROWS_1][INPUT_COLS_1])
 {
-    float layer1[NUM_FILTERS_1][OUTPUT_ROWS_1][OUTPUT_COLS_1]; // convolution 1
-    // float layer2[16][10][10];                                  // max_pooling
+    float layer1[NUM_FILTERS_1][OUTPUT_ROWS_1][OUTPUT_COLS_1]; // convolution (6*28*28)
+    float layer2[NUM_FILTERS_2][OUTPUT_ROWS_2][OUTPUT_COLS_2]; // max_pooling (6*14*14)
+    float layer3[NUM_FILTERS_3][OUTPUT_ROWS_3][OUTPUT_COLS_3]; // convolution (16*10*10)
+    float layer4[NUM_FILTERS_4][OUTPUT_ROWS_4][OUTPUT_COLS_4]; // max_pooling (16*5*5)
+    float layer5[NUM_FILTERS_5];                               // convolution + flatten (1*120)
+    float layer6[NUM_FILTERS_6];                               // fully connected (1*84)
+    float layer7[NUM_FILTERS_7];                               // fully connected (1*10)
+    float output[NUM_FILTERS_7];
 
-    layer_1_conv(input,model,layer1);
+    layer_1_conv(input, model, layer1);
 
-    // Max pooling layer 2
-    // for (int i = 0; i < 16; i++)
-    // {
-    //     for (int j = 0; j < 10; j++)
-    //     {
-    //         for (int k = 0; k < 10; k++)
-    //         {
-    //             float inputPatch[2][2];
-    //             for (int x = 0; x < 2; x++)
-    //             {
-    //                 for (int y = 0; y < 2; y++)
-    //                 {
-    //                     inputPatch[x][y] = layer1[i][j * 2 + x][k * 2 + y];
-    //                 }
-    //             }
-    //             layer2[i][j][k] = maxPooling(inputPatch);
-    //         }
-    //     }
-    // }
+    layer_2_subsampling(layer1, model, layer2);
+
+    layer_3_conv(layer2, model, layer3);
+
+    layer_4_subsampling(layer3, model, layer4);
+
+    layer_5_conv_flat(layer4, model, layer5);
+
+    layer_6_fc(layer5, model, layer6);
+
+    layer_7_fc(layer6, model, layer7);
+
+    log_softmax(layer7, NUM_FILTERS_7, output);
+
+    int max_ind = 0;
+    printf("%f,", output[0]);
+    for (int i = 1; i < NUM_FILTERS_7; i++)
+    {
+        printf("%f,", output[i]);
+        if (output[max_ind] < output[i])
+        {
+            max_ind = i;
+        }
+    }
+    printf("\nThe number is = %d\n", max_ind);
+
+    FILE *file = fopen("fc_2_weights.txt", "wb");
+    for (int i = 0; i < WEIGHT_ROWS_7; i++)
+    {
+        for (int m = 0; m < WEIGHT_COLS_7; m++)
+        {
+            fprintf(file, "%f,", model->weights5[i][m]);
+        }
+        fprintf(file, "\n");
+    }
+    file = fopen("fc_2.txt", "wb");
+    for (int i = 0; i < NUM_FILTERS_7; i++)
+    {
+        fprintf(file, "%f,", layer7[i]);
+    }
 }
 
 int main()
 {
     LeNet5 model;
     initLeNet5(&model);
+    printf("SUCCESS: Weights and biases have been loaded\n");
 
-    printf("Weights and biases have been loaded\n");
-
-    float input[INPUT_ROWS_1][INPUT_COLS_1] = {};
-    loadDataset(input,"mnist_test.csv");
+    float input[INPUT_ROWS_1][INPUT_COLS_1] = {}; // Padding : 2
+    loadDataset(input, "mnist_test.csv");
+    printf("SUCCESS: Dataset has been loaded into memory\n");
 
     forwardPass(&model, input);
+    printf("SUCCESS: Forward pass complete\n");
 
     return 0;
 }
--- cnn.h
+++ cnn.h
@@ -20,9 +20,9 @@
 
     LeNet-5 CNN Architecture:
         1) The input layer image is 1x1x28x28
-        2) The first hidden layer is a convolution layer [p=2,s=1,f=5,a=tanh] which outputs 1x1x28x28
+        2) The first hidden layer is a convolution layer [p=2,s=1,f=5,a=tanh] which outputs 1x6x28x28
         3) The second layer is a subsampling [p=0,s=2,f=2,a=avg_pool] which outputs 1x6x14x14
-        4) The third layer is a convolution [p=0,s=1,f=5,a=tanh] which outputs 1x6x10x10
+        4) The third layer is a convolution [p=0,s=1,f=5,a=tanh] which outputs 1x16x10x10
         5) The fourth layer is a subsampling [p=0,s=2,f=2,a=avg_pool] which outputs 1x16x5x5
         6) The fifth layer is a convolution [p=0,s=1,f=5,a=tanh] which outputs 1x120x1x1
         7) We will flatten the 1x120x1x1 feature maps into 1x120
@@ -45,7 +45,7 @@
 
 //-------------------------------------------------------------MACROS/GLOBALS---------------------------------------------------//
 
-#define MAX_LINE_SIZE 100000
+#define MAX_LINE_SIZE 5000000
 
 #define COMPUTE_OUTPUT_SIZE(N_in, padding, filter_size, stride) \
     (int)floor(((N_in) + 2 * (padding) - (filter_size)) / (float)(stride) + 1)
@@ -59,8 +59,8 @@
 #define STRIDE_1 1
 #define PADDING_1 0
 #define NUM_FILTERS_1 6
-#define OUTPUT_ROWS_1 COMPUTE_OUTPUT_SIZE(INPUT_ROWS_1, PADDING_1, KERNEL_SIZE_1, STRIDE_1)
-#define OUTPUT_COLS_1 COMPUTE_OUTPUT_SIZE(INPUT_COLS_1, PADDING_1, KERNEL_SIZE_1, STRIDE_1)
+#define OUTPUT_ROWS_1 28
+#define OUTPUT_COLS_1 28
 
 // Layer 2 - Average Pooling
 #define INPUT_ROWS_2 OUPUT_ROWS_1
@@ -69,8 +69,8 @@
 #define STRIDE_2 2
 #define PADDING_2 0
 #define NUM_FILTERS_2 6
-#define OUTPUT_ROWS_2 COMPUTE_OUTPUT_SIZE(INPUT_ROWS_2, PADDING_2, KERNEL_SIZE_2, STRIDE_2)
-#define OUTPUT_COLS_2 COMPUTE_OUTPUT_SIZE(INPUT_COLS_2, PADDING_2, KERNEL_SIZE_2, STRIDE_2)
+#define OUTPUT_ROWS_2 14
+#define OUTPUT_COLS_2 14
 
 // Layer 3 - convolution + tanh
 #define INPUT_ROWS_3 OUPUT_ROWS_2
@@ -79,8 +79,8 @@
 #define STRIDE_3 1
 #define PADDING_3 0
 #define NUM_FILTERS_3 16
-#define OUTPUT_ROWS_3 COMPUTE_OUTPUT_SIZE(INPUT_ROWS_3, PADDING_3, KERNEL_SIZE_3, STRIDE_3)
-#define OUTPUT_COLS_3 COMPUTE_OUTPUT_SIZE(INPUT_COLS_3, PADDING_3, KERNEL_SIZE_3, STRIDE_3)
+#define OUTPUT_ROWS_3 10
+#define OUTPUT_COLS_3 10
 
 // Layer 4 - Average Pooling
 #define INPUT_ROWS_4 OUPUT_ROWS_3
@@ -89,8 +89,8 @@
 #define STRIDE_4 2
 #define PADDING_4 0
 #define NUM_FILTERS_4 16
-#define OUTPUT_ROWS_4 COMPUTE_OUTPUT_SIZE(INPUT_ROWS_4, PADDING_4, KERNEL_SIZE_4, STRIDE_4)
-#define OUTPUT_COLS_4 COMPUTE_OUTPUT_SIZE(INPUT_COLS_4, PADDING_4, KERNEL_SIZE_4, STRIDE_4)
+#define OUTPUT_ROWS_4 5
+#define OUTPUT_COLS_4 5
 
 // Layer 5 - convolution + tanh
 #define INPUT_ROWS_5 OUPUT_ROWS_4
@@ -99,65 +99,71 @@
 #define STRIDE_5 1
 #define PADDING_5 0
 #define NUM_FILTERS_5 120
-#define OUTPUT_ROWS_5 COMPUTE_OUTPUT_SIZE(INPUT_ROWS_5, PADDING_5, KERNEL_SIZE_5, STRIDE_5)
-#define OUTPUT_COLS_5 COMPUTE_OUTPUT_SIZE(INPUT_COLS_5, PADDING_5, KERNEL_SIZE_5, STRIDE_5)
+#define OUTPUT_ROWS_5 1
+#define OUTPUT_COLS_5 1
 
 // NOTE: We will Flatten the layers before passing to Fully Connected Layers
 
 // Layer 6 - Fully Connected + tanh
 #define INPUT_ROWS_6 1
 #define INPUT_COLS_6 120
-#define WEIGHT_ROWS_6 120
-#define WEIGHT_COLS_6 1
+#define WEIGHT_ROWS_6 84
+#define WEIGHT_COLS_6 120
 #define NUM_FILTERS_6 84
 
 // Layer 7 - Fully Connected + softmax
 #define INPUT_ROWS_7 1
 #define INPUT_COLS_7 84
-#define WEIGHT_ROWS_7 84
-#define WEIGHT_COLS_7 1
+#define WEIGHT_ROWS_7 10
+#define WEIGHT_COLS_7 84
 #define NUM_FILTERS_7 10
 
 // Define the LeNet-5 architecture
 typedef struct
 {
-    //conv 1 (6x5x5)
+    // conv 1 (6x5x5)
     float weights1[NUM_FILTERS_1][KERNEL_SIZE_1][KERNEL_SIZE_1];
     float biases1[NUM_FILTERS_1];
-    //conv 2 (16x5x5)
-    float weights2[NUM_FILTERS_3][KERNEL_SIZE_3][KERNEL_SIZE_3];
+    // conv 2 (16x5x5)
+    float weights2[NUM_FILTERS_3][NUM_FILTERS_1][KERNEL_SIZE_3][KERNEL_SIZE_3];
     float biases2[NUM_FILTERS_3];
-    //conv 3 (120x5x5)
-    float weights3[NUM_FILTERS_5][KERNEL_SIZE_5][KERNEL_SIZE_5];
+    // conv 3 (120x5x5)
+    float weights3[NUM_FILTERS_5][NUM_FILTERS_3][KERNEL_SIZE_5][KERNEL_SIZE_5];
     float biases3[NUM_FILTERS_5];
-    //fc 4 (84x120x1)
-    float weights4[NUM_FILTERS_6][WEIGHT_ROWS_6];
+    // fc 4 (84x120x1)
+    float weights4[WEIGHT_ROWS_6][WEIGHT_COLS_6];
     float biases4[NUM_FILTERS_6];
-    //fc 5 (10x84x1)
-    float weights5[NUM_FILTERS_7][WEIGHT_ROWS_7];
+    // fc 5 (10x84x1)
+    float weights5[WEIGHT_ROWS_7][WEIGHT_COLS_7];
     float biases5[NUM_FILTERS_7];
 } LeNet5;
 
-// We load an image now, In the future we can load multiple images
-void loadDataset(float input[INPUT_ROWS_1][INPUT_COLS_1],const char* filename){
-    
-    FILE* file = fopen(filename,"rb");
-    
-    if(file == NULL){
+void loadDataset(float input[INPUT_ROWS_1][INPUT_COLS_1], const char *filename)
+{
+
+    FILE *file = fopen(filename, "rb");
+
+    if (file == NULL)
+    {
         printf("Failed to open the file\n");
         return;
     }
 
     char line[MAX_LINE_SIZE];
-    if(fgets(line,sizeof(line),file)){
-        
-        char* token;
-        token = strtok(line, ",");   //SKIP LABEL
-        token = strtok(NULL,",");
-        for(int i=2; i<INPUT_ROWS_1-2; i++){
-            for(int j=2; j<INPUT_COLS_1-2; j++){
-                input[i][j] = atof(token)/255;
-                token = strtok(NULL,",");
+    // fgets(line, sizeof(line), file); // 1
+    // fgets(line, sizeof(line), file); // 9
+    if (fgets(line, sizeof(line), file))
+    {
+
+        char *token;
+        token = strtok(line, ","); // SKIP LABEL
+        token = strtok(NULL, ",");
+        for (int i = 2; i < INPUT_ROWS_1 - 2; i++)
+        {
+            for (int j = 2; j < INPUT_COLS_1 - 2; j++)
+            {
+                input[i][j] = atof(token) / 255;
+                token = strtok(NULL, ",");
             }
         }
     }
@@ -165,7 +171,6 @@ void loadDataset(float input[INPUT_ROWS_1][INPUT_COLS_1],const char* filename){
     fclose(file);
 }
 
-// Initialize the LeNet-5 model
 void initLeNet5(LeNet5 *model)
 {
     FILE *file = fopen("parameters.txt", "rb");
@@ -178,11 +183,12 @@ void initLeNet5(LeNet5 *model)
     char line[MAX_LINE_SIZE];
 
     // Read weights of layer 1
-    if(fgets(line,MAX_LINE_SIZE,file)){
-        
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+
+        char *token;
         token = strtok(line, " ");
-     
+
         for (int c = 0; c < NUM_FILTERS_1; c++)
         {
             for (int i = 0; i < KERNEL_SIZE_1; i++)
@@ -190,150 +196,282 @@ void initLeNet5(LeNet5 *model)
                 for (int j = 0; j < KERNEL_SIZE_1; j++)
                 {
                     model->weights1[c][i][j] = atof(token);
-                    token = strtok(NULL," ");
+                    token = strtok(NULL, " ");
                 }
             }
-        }   
+        }
     }
 
     // Read biases of layer 1
-    if(fgets(line, MAX_LINE_SIZE,file)){
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
         for (int c = 0; c < NUM_FILTERS_1; c++)
         {
             model->biases1[c] = atof(token);
-            token = strtok(NULL," ");
+            token = strtok(NULL, " ");
         }
     }
 
     // Read weights of layer 3
-    if(fgets(line,MAX_LINE_SIZE,file)){
-        
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
-     
+
         for (int c = 0; c < NUM_FILTERS_3; c++)
         {
-            for (int i = 0; i < KERNEL_SIZE_3; i++)
+            for (int k = 0; k < NUM_FILTERS_1; k++)
             {
-                for (int j = 0; j < KERNEL_SIZE_3; j++)
+                for (int i = 0; i < KERNEL_SIZE_3; i++)
                 {
-                    model->weights2[c][i][j] = atof(token);
-                    token = strtok(NULL," ");
+                    for (int j = 0; j < KERNEL_SIZE_3; j++)
+                    {
+                        model->weights2[c][k][i][j] = atof(token);
+                        token = strtok(NULL, " ");
+                    }
                 }
             }
-        }   
+        }
     }
 
     // Read biases of layer 3
-    if(fgets(line, MAX_LINE_SIZE,file)){
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
         for (int c = 0; c < NUM_FILTERS_3; c++)
         {
             model->biases2[c] = atof(token);
-            token = strtok(NULL," ");
+            token = strtok(NULL, " ");
         }
     }
 
     // Read weights of layer 5
-    if(fgets(line,MAX_LINE_SIZE,file)){
-        
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
-     
+
         for (int c = 0; c < NUM_FILTERS_5; c++)
         {
-            for (int i = 0; i < KERNEL_SIZE_5; i++)
+            for (int k = 0; k < NUM_FILTERS_3; k++)
             {
-                for (int j = 0; j < KERNEL_SIZE_5; j++)
+                for (int i = 0; i < KERNEL_SIZE_5; i++)
                 {
-                    model->weights3[c][i][j] = atof(token);
-                    token = strtok(NULL," ");
+                    for (int j = 0; j < KERNEL_SIZE_5; j++)
+                    {
+                        model->weights3[c][k][i][j] = atof(token);
+                        token = strtok(NULL, " ");
+                    }
                 }
             }
-        }   
+        }
     }
 
     // Read biases of layer 5
-    if(fgets(line, MAX_LINE_SIZE,file)){
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
         for (int c = 0; c < NUM_FILTERS_5; c++)
         {
             model->biases3[c] = atof(token);
-            token = strtok(NULL," ");
+            token = strtok(NULL, " ");
         }
-    }    
-
+    }
 
     // Read weights of layer 6
-    if(fgets(line,MAX_LINE_SIZE,file)){
-        
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+
+        char *token;
         token = strtok(line, " ");
-     
-        for (int c = 0; c < NUM_FILTERS_6; c++)
+
+        for (int c = 0; c < WEIGHT_ROWS_6; c++)
         {
-            for (int i = 0; i < WEIGHT_ROWS_6; i++)
+            for (int i = 0; i < WEIGHT_COLS_6; i++)
             {
                 model->weights4[c][i] = atof(token);
-                token = strtok(NULL," ");
+                token = strtok(NULL, " ");
             }
-        }   
+        }
     }
 
     // Read biases of layer 6
-    if(fgets(line, MAX_LINE_SIZE,file)){
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
-        for (int c = 0; c < NUM_FILTERS_6; c++)
+        for (int c = 0; c < WEIGHT_ROWS_6; c++)
         {
             model->biases4[c] = atof(token);
-            token = strtok(NULL," ");
+            token = strtok(NULL, " ");
+        }
+    }
+    printf("OK\n");
+
+    // Read weights of layer 7
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+
+        char *token;
+        token = strtok(line, " ");
+
+        for (int c = 0; c < WEIGHT_ROWS_7; c++)
+        {
+            for (int i = 0; i < WEIGHT_COLS_7; i++)
+            {
+                model->weights5[c][i] = atof(token);
+                token = strtok(NULL, " ");
+            }
         }
     }
 
-    // Read weights of layer 7
-    if(fgets(line,MAX_LINE_SIZE,file)){
-        
-        char* token;
-        token = strtok(line, " ");
-     
-        for (int c = 0; c < NUM_FILTERS_7; c++)
-        {
-            for (int i = 0; i < WEIGHT_ROWS_7; i++)
-            {
-                model->weights5[c][i] = atof(token);
-                token = strtok(NULL," ");
-            }
-        }   
-    }
-
     // Read biases of layer 7
-    if(fgets(line, MAX_LINE_SIZE,file)){
-        char* token;
+    if (fgets(line, MAX_LINE_SIZE, file))
+    {
+        char *token;
         token = strtok(line, " ");
-        for (int c = 0; c < NUM_FILTERS_7; c++)
+        for (int c = 0; c < WEIGHT_ROWS_7; c++)
         {
             model->biases5[c] = atof(token);
-            token = strtok(NULL," ");
+            token = strtok(NULL, " ");
         }
     }
 
     fclose(file);
 }
 
-void layer_2_subsampling(double input_matrix[INPUT_ROWS_1][INPUT_COLS_1],
-                 LeNet5* model, double layer1[NUM_FILTERS_1][OUTPUT_ROWS_1][OUTPUT_COLS_1])
+void layer_7_fc(float input_layer[NUM_FILTERS_6], LeNet5 *model, float output_layer[NUM_FILTERS_7])
 {
-    for(int filter_no = 0;filter_no < NUM_FILTERS_1;filter_no++){
+    for (int n = 0; n < WEIGHT_ROWS_7; n++)
+    {
+        output_layer[n] = model->biases5[n];
 
+        for (int i = 0; i < WEIGHT_COLS_7; i++)
+        {
+            output_layer[n] += input_layer[i] * model->weights5[n][i];
+        }
     }
 }
 
-void layer_1_conv(float input_matrix[INPUT_ROWS_1][INPUT_COLS_1], LeNet5* model, float layer1[NUM_FILTERS_1][OUTPUT_ROWS_1][OUTPUT_COLS_1])
+void layer_6_fc(float input_layer[NUM_FILTERS_5], LeNet5 *model, float output_layer[NUM_FILTERS_6])
+{
+    for (int n = 0; n < WEIGHT_ROWS_6; n++)
+    {
+        output_layer[n] = model->biases4[n];
+
+        for (int i = 0; i < WEIGHT_COLS_6; i++)
+        {
+            output_layer[n] += input_layer[i] * model->weights4[n][i];
+        }
+
+        output_layer[n] = tanh(output_layer[n]);
+    }
+}
+
+void layer_5_conv_flat(float input_layer[NUM_FILTERS_4][OUTPUT_ROWS_4][OUTPUT_COLS_4], LeNet5 *model, float output_layer[NUM_FILTERS_5])
+{
+    // printf("Input dim = 1x%dx%dx%d . Output dim = 1x%d", NUM_FILTERS_4, OUTPUT_ROWS_4, OUTPUT_COLS_4, NUM_FILTERS_5);
+    for (int k = 0; k < NUM_FILTERS_5; k++)
+    {
+        output_layer[k] = model->biases3[k];
+
+        for (int c = 0; c < NUM_FILTERS_4; c++)
+        {
+            for (int m = 0; m < KERNEL_SIZE_5; m++)
+            {
+                for (int n = 0; n < KERNEL_SIZE_5; n++)
+                {
+                    output_layer[k] += input_layer[c][m][n] * model->weights3[k][c][m][n];
+                }
+            }
+        }
+
+        output_layer[k] = tanh(output_layer[k]);
+    }
+}
+
+void layer_4_subsampling(float input_layer[NUM_FILTERS_3][OUTPUT_ROWS_3][OUTPUT_COLS_3], LeNet5 *model, float output_layer[NUM_FILTERS_4][OUTPUT_ROWS_4][OUTPUT_COLS_4])
+{
+    for (int filter_no = 0; filter_no < NUM_FILTERS_4; filter_no++)
+    {
+        for (int i = 0; i < OUTPUT_ROWS_4; i++)
+        {
+            for (int j = 0; j < OUTPUT_COLS_4; j++)
+            {
+                float sum = 0.0;
+
+                // Stride : 2 (So we divide Kernel by 2)
+                for (int m = 0; m < KERNEL_SIZE_4; m++)
+                {
+                    for (int n = 0; n < KERNEL_SIZE_4; n++)
+                    {
+                        sum += input_layer[filter_no][i * STRIDE_4 + m][j * STRIDE_4 + n];
+                    }
+                }
+
+                // Calculate the average by dividing the sum by the pool size
+                output_layer[filter_no][i][j] = sum / (KERNEL_SIZE_4 * KERNEL_SIZE_4);
+            }
+        }
+    }
+}
+
+void layer_3_conv(float input_layer[NUM_FILTERS_2][OUTPUT_ROWS_2][OUTPUT_COLS_2], LeNet5 *model, float output_layer[NUM_FILTERS_3][OUTPUT_ROWS_3][OUTPUT_COLS_3])
+{
+    for (int k = 0; k < NUM_FILTERS_3; k++)
+    {
+        for (int i = 0; i < OUTPUT_ROWS_3; i++)
+        {
+            for (int j = 0; j < OUTPUT_COLS_3; j++)
+            {
+                output_layer[k][i][j] = model->biases2[k];
+
+                for (int c = 0; c < NUM_FILTERS_2; c++)
+                {
+                    for (int m = 0; m < KERNEL_SIZE_3; m++)
+                    {
+                        for (int n = 0; n < KERNEL_SIZE_3; n++)
+                        {
+                            output_layer[k][i][j] += input_layer[c][i + m][j + n] * model->weights2[k][c][m][n];
+                        }
+                    }
+                }
+
+                output_layer[k][i][j] = tanh(output_layer[k][i][j]);
+            }
+        }
+    }
+}
+
+void layer_2_subsampling(float input_layer[NUM_FILTERS_1][OUTPUT_ROWS_1][OUTPUT_COLS_1], LeNet5 *model, float output_layer[NUM_FILTERS_2][OUTPUT_ROWS_2][OUTPUT_COLS_2])
+{
+    for (int filter_no = 0; filter_no < NUM_FILTERS_2; filter_no++)
+    {
+        for (int i = 0; i < OUTPUT_ROWS_2; i++)
+        {
+            for (int j = 0; j < OUTPUT_COLS_2; j++)
+            {
+                float sum = 0.0;
+
+                for (int m = 0; m < KERNEL_SIZE_2; m++)
+                {
+                    for (int n = 0; n < KERNEL_SIZE_2; n++)
+                    {
+                        sum += input_layer[filter_no][i * STRIDE_2 + m][j * STRIDE_2 + n];
+                    }
+                }
+
+                output_layer[filter_no][i][j] = sum / (KERNEL_SIZE_2 * KERNEL_SIZE_2);
+            }
+        }
+    }
+}
+
+void layer_1_conv(float input_layer[INPUT_ROWS_1][INPUT_COLS_1], LeNet5 *model, float output_layer[NUM_FILTERS_1][OUTPUT_ROWS_1][OUTPUT_COLS_1])
 {
     for (int filter_no = 0; filter_no < NUM_FILTERS_1; filter_no++)
     {
@@ -348,13 +486,12 @@ void layer_1_conv(float input_matrix[INPUT_ROWS_1][INPUT_COLS_1], LeNet5* model,
                 {
                     for (int kernel_col = 0; kernel_col < KERNEL_SIZE_1; kernel_col++)
                     {
-                        sum += input_matrix[input_row + kernel_row][input_col + kernel_col] * model->weights1[filter_no][kernel_row][kernel_col];
+                        sum += input_layer[input_row + kernel_row][input_col + kernel_col] * model->weights1[filter_no][kernel_row][kernel_col];
                     }
                 }
 
                 // Feature Map
-                // layer1[filter_no][input_row][input_col] = sum;
-                layer1[filter_no][input_row][input_col] = tanh((sum + model->biases1[filter_no]));
+                output_layer[filter_no][input_row][input_col] = tanh((sum + model->biases1[filter_no]));
             }
         }
     }
