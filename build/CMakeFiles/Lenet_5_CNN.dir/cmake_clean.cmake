b5b34b2afe8b0a49afe4bf1b1dbbd8a38b8ada5e
RNGesus-exe <behzadshaheryar@gmail.com>
2023-07-03 17:32:52 +0500

    Added CMake

---
+++ build/CMakeFiles/Lenet_5_CNN.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lenet_5_CNN.dir/src/cnn.c.o"
+  "CMakeFiles/Lenet_5_CNN.dir/src/cnn.c.o.d"
+  "Lenet_5_CNN"
+  "Lenet_5_CNN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lenet_5_CNN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Lenet_5_CNN.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lenet_5_CNN.
+# This may be replaced when dependencies are built.
